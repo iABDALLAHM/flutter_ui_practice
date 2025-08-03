74d5e322f779fac626a6a7f3b8851e9d9353115d
Abdallah Mohamed <115114629+AMOHAME0@users.noreply.github.com>
2025-08-04 02:13:30 +0300

    feat(variant_2): scaffold base structure with views and viewmodels

---
+++ login_screen/lib/features/auth_variants/variant_2/presentation/view/login_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:login_screen/features/auth_variants/variant_1/presentation/views/widgets/login_view_body.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: LoginViewBody());
+  }
+}
---
+++ login_screen/lib/features/auth_variants/variant_2/presentation/view/register_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:login_screen/features/auth_variants/variant_2/presentation/view/widgets/register_view_body.dart';
+
+class RegisterView extends StatelessWidget {
+  const RegisterView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: RegisterViewBody());
+  }
+}
