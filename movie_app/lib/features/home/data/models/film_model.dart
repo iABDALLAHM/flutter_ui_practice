3a57e448b0309388d0246c972182ed4d30d2377a
Abdallah Mohamed <115114629+AMOHAME0@users.noreply.github.com>
2025-09-16 00:37:56 +0300

    feat(models): add FilmModel with JSON serialization support

---
+++ movie_app/lib/features/home/data/models/film_model.dart
@@ -0,0 +1,32 @@
+class FilmModel {
+  final int filmId;
+  final String filmName;
+  final String posterUrl;
+  final double rating;
+  final List<String> genre;
+  final int length;
+  final String language;
+  final String description;
+  FilmModel({
+    required this.filmId,
+    required this.filmName,
+    required this.posterUrl,
+    required this.rating,
+    required this.genre,
+    required this.length,
+    required this.language,
+    required this.description,
+  });
+  factory FilmModel.fromJson(json) {
+    return FilmModel(
+      filmId: json["id"],
+      filmName: json["title"],
+      posterUrl: json["posterUrl"],
+      rating: json["rating"],
+      genre: json["genre"],
+      length: json["length"],
+      language: json["language"],
+      description: json["description"],
+    );
+  }
+}
